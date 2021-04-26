abb393299096d48532b76097284a7a77625b85ac
Daniel Gerlanc <dan@gerlanc.com>
2021-04-26 14:16:07 -0400

    Update to work on ARM

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ BUILD=$(date "+%Y-%m-%d")
 
 ROOT="$HOME/code"
 REPO="${ROOT}/emacs-native"
+BREW_ROOT="/opt/homebrew"
 
 if [ ! -d "$ROOT" ]; then
     echo "Creating ${ROOT}"
@@ -23,10 +24,10 @@ else
 fi
 
 # TODO: Make gcc location user-configurable
-export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:${PATH}"
-export CFLAGS="-I/usr/local/Cellar/gcc/10.2.0/include -O3 -march=native"
-export LDFLAGS="-L/usr/local/Cellar/gcc/10.2.0/lib/gcc/10 -I/usr/local/Cellar/gcc/10.2.0/include"
-export LIBRARY_PATH="/usr/local/Cellar/gcc/10.2.0/lib/gcc/10:${LIBRARY_PATH:-}"
+export PATH="$BREW_ROOT/opt/gnu-sed/libexec/gnubin:${PATH}"
+export CFLAGS="-I$BREW_ROOT/Cellar/gcc/10.2.0/include -O3"
+export LDFLAGS="-L$BREW_ROOT/Cellar/gcc/10.2.0/lib/gcc/10 -I$BREW_ROOT/Cellar/gcc/10.2.0/include"
+export LIBRARY_PATH="$BREW_ROOT/Cellar/gcc/10.2.0/lib/gcc/10:${LIBRARY_PATH:-}"
 
 cd $REPO || exit
 
@@ -35,11 +36,8 @@ git clean -xfd
 ./autogen.sh
 
 ./configure \
-     --prefix=/usr/local/opt/gccemacs-${BUILD} \
-     --enable-locallisppath=/usr/local/opt/gccemacs-${BUILD}/share/emacs/28.0.50/site-lisp \
      --with-ns \
-     --disable-ns-self-contained \
-     --with-nativecomp \
+     --with-native-compilation \
      --with-cairo \
      --with-threads \
      --with-modules \
@@ -57,23 +55,3 @@ make -j${N_CORES} NATIVE_FULL_AOT=1 BYTE_COMPILE_EXTRA_FLAGS='--eval "(setq comp
 
 make install-eln
 make install
-
-# Move Emacs.app to /Applications
-if [ -d "${REPO}/nextstep/Emacs.app" ]; then
-    cp -a "${REPO}/nextstep/Emacs.app" "/Applications/Emacs-${BUILD}.app"
-
-    cd /Applications/Emacs-${BUILD}.app/Contents || exit
-    ln -s /usr/local/opt/gccemacs-${BUILD}/lib/emacs/28.0.50/native-lisp .
-    ln -s /usr/local/opt/gccemacs-${BUILD}/share/emacs/28.0.50/lisp .
-fi
-
-# Replace symbolic links in /usr/local/bin
-NEW_BIN_DIR="/usr/local/opt/gccemacs-${BUILD}/bin"
-if [ -e "$NEW_BIN_DIR" ]; then
-    echo "Replacing symbolic links in /usr/local/bin"
-    cd /usr/local/bin || exit
-    rm emacs
-    rm emacsclient
-    ln -s "${NEW_BIN_DIR}/emacs" .
-    ln -s "${NEW_BIN_DIR}/emacsclient" .
-fi
